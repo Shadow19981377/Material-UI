16a468bdbcb1acad560c0cf14a060963ff621542
Shadow19981377 <sha1234dow@gmail.com>
2023-09-27 15:05:28 +0330

    add new list item

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,17 +7,13 @@ import {
   ListItemText,
 } from "@mui/material";
 // import InboxIcon from '@mui/icons-material/Inbox';
-import { Home, Article, Groups } from "@mui/icons-material";
+import { Home, Article, Groups, Storefront } from "@mui/icons-material";
 
 import React from "react";
 
 const Sidebar = () => {
   return (
-    <Box    
-      flex={1}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
+    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
       <List>
         <ListItem disablePadding>
           <ListItemButton component="a" href="#home">
@@ -41,6 +37,14 @@ const Sidebar = () => {
               <Groups />
             </ListItemIcon>
             <ListItemText primary="Groups" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#marketplace">
+            <ListItemIcon>
+              <Storefront />
+            </ListItemIcon>
+            <ListItemText primary="Marketplace" />
           </ListItemButton>
         </ListItem>
       </List>
